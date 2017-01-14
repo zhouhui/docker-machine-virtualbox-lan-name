fc800bb66dc9cf9f4ee1649634ac1a201bdbe9e2
zhouhui <zhou.hui@outlook.com>
2017-01-14 17:34:52 +0800

    update for adding comments

--- start.wrapper.sh
+++ start.wrapper.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 export WRAPPER_PATH=`pwd`
+
+# use this one instead of default ~/.docker if you want
 export MACHINE_STORAGE_PATH="/d/workspaces/virtualbox/docker"
+
+# prevent docker-machine from starting virtualbox dhcp service
 export VIRTUALBOX_HOSTONLY_NO_DHCP=true
+
+# please refer to docker-machine's doc to use env variables you want
 # export VIRTUALBOX_SHARE_FOLDER=""
+
+# make virtualbox shell commands be able to access
 export PATH=$PATH:$VBOX_MSI_INSTALL_PATH
 
-
+# cheet docker-machine
+# if you want to use original docker-machine please type 'Docker-machine' instead
+# and the docker-machine.exe is a cheet script which is also used in start.sh of Docker Toolbox
 docker-machine () {
   $WRAPPER_PATH/docker-machine.exe "$@"
 }
 
 export -f docker-machine
+
+# always using original docker-machine
+Docker-machine () {
+  "$(echo $DOCKER_TOOLBOX_INSTALL_PATH/docker-machine.exe)" "$@"
+}
+
+export -f Docker-machine
 
 . "/c/Program Files/Docker Toolbox/start.sh"
 
